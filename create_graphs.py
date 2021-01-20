8a21f265c01ec44bbb0d26360d98a6a62c237ca2
idimi <ilias.dimitriadis@gmail.com>
2021-01-20 14:25:12 +0200

    comment out not used funcs

--- create_graphs.py
+++ create_graphs.py
@@ -26,80 +26,80 @@ def get_date_of_oldest_newest_document(col):
     newest_doc_sate = (iterator[0]['created_at'])
     return (oldest_doc_date,newest_doc_sate)
 
-def get_temporal_weighted_graphs(col):
-    G=nx.Graph()
-    start,end = get_date_of_oldest_newest_document(col)
-    # print (start,end)
-    until = start + timedelta(days=PER_DAYS)
-    until = datetime.combine(until.date(), datetime.min.time())
-    while until.date() <= end.date():
-        users=set()
-        notInDb=set()
-        it = col.find({"created_at": {"$lte": until}})
-        for k in it:
-            userid = k['user']['id_str']
-            users.add(userid)
-        print('users in our DB until: '+str(until.strftime('%m_%d_%Y'))+' are: '+ str(len(users)))
-        G.add_nodes_from(list(users))
-        iterator = col.find({"created_at": {"$lte": until,"$gte":start}})
-        for i in iterator:
-            user = i['user']['id_str']
-            entities = i['entities']
-            if 'user_mentions' in entities:
-                for m in entities['user_mentions']:
-                    if G.has_edge(user, m['id_str']):
-                        G[user][m['id_str']]['weight'] += 1.0
-                    else:
-                        G.add_edge(user, m['id_str'], weight=1.0)
-                        if m['id_str'] not in users:
-                            notInDb.add(m['id_str'])
-        G.remove_edges_from(G.selfloop_edges())
-        pickle.dump(G, open('graphs/weighted_' + str(until.strftime('%m_%d_%Y')), 'wb'))
-        pickle.dump(users, open('users/allUsers_until_' + str(until.strftime('%m_%d_%Y')), 'wb'))
-        until = until + timedelta(days=PER_DAYS)
-        print('users in our DB:', len(users))
-        print('users not in our DB:', len(notInDb))
+# def get_temporal_weighted_graphs(col):
+#     G=nx.Graph()
+#     start,end = get_date_of_oldest_newest_document(col)
+#     # print (start,end)
+#     until = start + timedelta(days=PER_DAYS)
+#     until = datetime.combine(until.date(), datetime.min.time())
+#     while until.date() <= end.date():
+#         users=set()
+#         notInDb=set()
+#         it = col.find({"created_at": {"$lte": until}})
+#         for k in it:
+#             userid = k['user']['id_str']
+#             users.add(userid)
+#         print('users in our DB until: '+str(until.strftime('%m_%d_%Y'))+' are: '+ str(len(users)))
+#         G.add_nodes_from(list(users))
+#         iterator = col.find({"created_at": {"$lte": until,"$gte":start}})
+#         for i in iterator:
+#             user = i['user']['id_str']
+#             entities = i['entities']
+#             if 'user_mentions' in entities:
+#                 for m in entities['user_mentions']:
+#                     if G.has_edge(user, m['id_str']):
+#                         G[user][m['id_str']]['weight'] += 1.0
+#                     else:
+#                         G.add_edge(user, m['id_str'], weight=1.0)
+#                         if m['id_str'] not in users:
+#                             notInDb.add(m['id_str'])
+#         G.remove_edges_from(G.selfloop_edges())
+#         pickle.dump(G, open('graphs/weighted_' + str(until.strftime('%m_%d_%Y')), 'wb'))
+#         pickle.dump(users, open('users/allUsers_until_' + str(until.strftime('%m_%d_%Y')), 'wb'))
+#         until = until + timedelta(days=PER_DAYS)
+#         print('users in our DB:', len(users))
+#         print('users not in our DB:', len(notInDb))
 
-'''CREATES AND STORES PICKLE FILES OF DELTA TIME WISE GRAPHS. EACH GRAPH HAS INFORMATION FOR ALL THE USERS AND THEIR ACTIVITY IN THIS
-SPECIFIC TIME DELTA.'''
-def get_delta_graphs(col):
-    start, end = get_date_of_oldest_newest_document(col)
-    until = start + timedelta(days=PER_DAYS)
-    until = datetime.combine(until.date(), datetime.min.time())
+# '''CREATES AND STORES PICKLE FILES OF DELTA TIME WISE GRAPHS. EACH GRAPH HAS INFORMATION FOR ALL THE USERS AND THEIR ACTIVITY IN THIS
+# SPECIFIC TIME DELTA.'''
+# def get_delta_graphs(col):
+#     start, end = get_date_of_oldest_newest_document(col)
+#     until = start + timedelta(days=PER_DAYS)
+#     until = datetime.combine(until.date(), datetime.min.time())
 
-    while until.date() <= end.date():
-        since = until - timedelta(days=PER_DAYS)
-        GG=pickle.load(open('graphs/weighted_' + str(until.strftime('%m_%d_%Y')), 'rb'))
-        userInfo = {}
-        for node in tqdm(G.nodes()):
-            userInfo[node]={'hashtags':[],'urls':[],'tweets':0 ,'degree': str(G.degree[node]), 'weight': str(int(G.degree(weight='weight')[node])),'label':str(node)}
-        print (userInfo)
-        it = col.find({"created_at": {"$lte": until,"$gte":since}})
-        users=set()
-        for j in it:
-            user=j['user']['id_str']
-            users.add(user)
-        for u in tqdm(list(users)):
-            iterator = col.find({"$and": [{'user.id_str': u}, {"created_at": {"$lte": until, "$gte": since}}]}).sort('created_at', 1)
-            userTags=[]
-            tweets=0
-            userUrls=[]
-            for i in iterator:
-                tweets += 1
-                if 'entities' in i:
-                    for h in i['entities']['hashtags']:
-                        foundhashtag = h['text'].lower()
-                        # print (foundhashtag)
-                        userTags.append(foundhashtag)
-                    for n in i['entities']['urls']:
-                        foundurl = (n['expanded_url'])
-                        # print (foundurl)
-                        userUrls.append(foundurl)
-            obj={'hashtags':userTags,'urls':userUrls,'tweets':tweets,'degree':str(G.degree[u]),'weight':str(int(G.degree(weight='weight')[u])),'label':u}
-            userInfo[u] = obj
-        nx.set_node_attributes(G, userInfo)
-        pickle.dump(G,open('graphs/delta_'+ str(until.strftime('%m_%d_%Y')), 'wb'))
-        until = until + timedelta(days=PER_DAYS)
+#     while until.date() <= end.date():
+#         since = until - timedelta(days=PER_DAYS)
+#         GG=pickle.load(open('graphs/weighted_' + str(until.strftime('%m_%d_%Y')), 'rb'))
+#         userInfo = {}
+#         for node in tqdm(G.nodes()):
+#             userInfo[node]={'hashtags':[],'urls':[],'tweets':0 ,'degree': str(G.degree[node]), 'weight': str(int(G.degree(weight='weight')[node])),'label':str(node)}
+#         print (userInfo)
+#         it = col.find({"created_at": {"$lte": until,"$gte":since}})
+#         users=set()
+#         for j in it:
+#             user=j['user']['id_str']
+#             users.add(user)
+#         for u in tqdm(list(users)):
+#             iterator = col.find({"$and": [{'user.id_str': u}, {"created_at": {"$lte": until, "$gte": since}}]}).sort('created_at', 1)
+#             userTags=[]
+#             tweets=0
+#             userUrls=[]
+#             for i in iterator:
+#                 tweets += 1
+#                 if 'entities' in i:
+#                     for h in i['entities']['hashtags']:
+#                         foundhashtag = h['text'].lower()
+#                         # print (foundhashtag)
+#                         userTags.append(foundhashtag)
+#                     for n in i['entities']['urls']:
+#                         foundurl = (n['expanded_url'])
+#                         # print (foundurl)
+#                         userUrls.append(foundurl)
+#             obj={'hashtags':userTags,'urls':userUrls,'tweets':tweets,'degree':str(G.degree[u]),'weight':str(int(G.degree(weight='weight')[u])),'label':u}
+#             userInfo[u] = obj
+#         nx.set_node_attributes(G, userInfo)
+#         pickle.dump(G,open('graphs/delta_'+ str(until.strftime('%m_%d_%Y')), 'wb'))
+#         until = until + timedelta(days=PER_DAYS)
 
 '''CREATES AND STORES PICKLE FILES OF CUMULATIVE GRAPHS. EACH GRAPH HAS INFORMATION FOR ALL THE USERS AND THEIR ACTIVITY UP TO THAT TIME'''
 def get_cumulative_graphs(col):
